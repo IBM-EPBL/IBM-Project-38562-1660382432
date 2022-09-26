fb80130e706b25cf65f158149e7e5013a231defb
Kishan <104731166+IamKishan08@users.noreply.github.com>
2022-09-26 20:15:17 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function calculateDays() {
-  var date1 = new Date(document.getElementById("date1").value);
-  var date2 = new Date(document.getElementById("date2").value);
-  var diff = Math.abs(date1.getTime() - date2.getTime());
-  var days = Math.ceil(diff / (1000 * 3600 * 24));
-  document.getElementById("result").innerHTML = days;
-}
